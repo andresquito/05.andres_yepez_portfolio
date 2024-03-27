5a0a0c248153a2457658dcf43ab3525f7d6313f1
andresquito <ricardoandre1@hotmail.com>
2024-03-27 12:14:32 -0500

    dist

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:'export',
-
+    distDir: 'dist',
+    //npm run build
+    //npx serve ./dist
+    
 };
 
 export default nextConfig;
