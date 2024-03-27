96e571a418f48c848fda42a18faef686f804565a
andresquito <ricardoandre1@hotmail.com>
2024-03-27 11:20:49 -0500

    yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:'export',
+    base:"https://andresquito.github.io/05.andres_yepez_portfolio"
 };
 
 export default nextConfig;
